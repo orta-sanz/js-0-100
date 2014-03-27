6e7496dd30c17726d68f8d5ba8d4ce943b76f133
orta-sanz <orta.sanz.93@gmail.com>
2014-03-27 10:38:12 +0100

    Terminado el Ejercicio DOM

--- 1-intro/dom/js/ejercicio.js
+++ 1-intro/dom/js/ejercicio.js
@@ -1,17 +1,39 @@
 var EJERCICIO = (function() {
 	function ordenar() {
-		// Ordena los diferentes span dentro del parrafo según la clase a la que pertenecen
+		var verses = document.querySelectorAll('span');
+		var orde_verses = [];
+		var par = document.body.querySelector('p');
 
+		while (par.firstChild) {
+			par.removeChild(par.firstChild);
+		}
+		
+		for (var i = 0; i <= verses.length; i++) {
+			for (var f = 0; f < verses.length; f++) {
+				if (((verses[f].classList[0]).split('-')[1]) == i) {
+					orde_verses.push(verses[f]);
+				}
+			}
+		}
+
+		for (var i = 0; i < orde_verses.length; i++) {
+			par.appendChild(orde_verses[i]);
+		}
 	}
 
 	function addSaltos() {
-		// Edita el estilo de los span o añade una clase para que se realice un salto de linea entre cada verso
-
+		var verses = document.querySelectorAll('span');
+		for (var i = 0; i < verses.length; i++) {
+			verses[i].classList.add('linea');
+		}
 	}
 
 	function addVerso(verso) {
-		// Añade el verso final de la poesía de Almafuerte
-
+		var par = document.body.querySelector('p');
+		var span = document.createElement('span');
+		span.classList.add('verso-14')
+		span.appendChild(document.createTextNode(verso));
+		par.appendChild(span);
 	}
 
 	return function() {
