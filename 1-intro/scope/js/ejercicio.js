9296bb040a1520115eb2c84ba95bd250e026276d
orta-sanz <orta.sanz.93@gmail.com>
2014-03-27 10:41:08 +0100

    Primera modificacion al segundo ejercicio

--- 1-intro/scope/js/ejercicio.js
+++ 1-intro/scope/js/ejercicio.js
@@ -2,11 +2,11 @@ function EJERCICIO() {
 	var funciones = [];
 	var salida = document.getElementById('salida');
 
-	// Las funciones almacenadas en este bucle no son las que deseamos
-	for(var i=1; i<=10; i++)
-		funciones.push(function() {
-			salida.innerHTML = salida.innerHTML+i+'&nbsp;';
+	while (funciones.length <= 10) {
+		funciones.push(function (numero){
+			salida.innerHTML += numero + ' ';
 		});
+	}
 
 	salida.innerText = '';
 	for(var f in funciones)
