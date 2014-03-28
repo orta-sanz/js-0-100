07b5d775e2b1a7e9997dae4b06a0a9222069636e
orta-sanz <orta.sanz.93@gmail.com>
2014-03-28 09:30:55 +0100

    Fuera comentarios. Ejercicio Funciones

--- 1-intro/function/js/ejercicio.js
+++ 1-intro/function/js/ejercicio.js
@@ -9,7 +9,6 @@ var EJERCICIO = (function() {
 	}
 
 	function tapaDeAPoco() {
-		//console.log(color().r);
 		context.beginPath();
 		context.rect(0, 0, width, height);
 		context.fillStyle = 'rgba(255, 255, 255, .1)';
@@ -29,14 +28,6 @@ var EJERCICIO = (function() {
 	setInterval(tapaDeAPoco, 250);
 
 	return function() {
-		/*
-		 * 1. Usa la función dibujaCirculo para dibujar todos los circulos pasados por parametro
-		 * 
-		 * 2. Si no se pasan parametros, dibuja un numero aleatorio de circulos, con un radio
-		 *    aleatorio y una posición aleatoria dentro del canvas
-		 *
-		 * 3. Dibuja cada circulo en un color aleatorio
-		 */
 		 if (arguments.length == 0) {
 		 	for (var i = 0; i <= aleatorio(100); i++) {
 		 		var x = aleatorio(400);
@@ -49,36 +40,12 @@ var EJERCICIO = (function() {
 		 	}
 		 }
 		 else {
-		 	/*
-		 	* The apply() method accepts two arguments: the value of this inside the function
-			* and an array of arguments. This second argument may be an instance of Array, but it can also be
-			* the arguments object.
-			* apply() es un método de Prototype. Llamandolo con la funcion dibujaCirculo nos permite
-			* pasarle dos tipos de argumentos, el valor de this dentro de la función y un ARRAY de ARGUMENTOS
-			*
-			* Ya que arguments es un array de elementos, podemos llamar a la función apply (prototype) de
-			* dibuja circulo
-		 	*/
 		 	for (var i = 0; i < arguments.length; i++) {
 		 		dibujaCirculo.apply(this, arguments[i]);
 		 	}
 		 }
-		 /*else {
-		 var x = arguments[0];
-		 	var y = arguments[1];
-		 	var radio;
-		 	if (arguments[2] != null) {
-		 		radio = arguments[2];
-		 	}
-		 	else {
-		 		radio = aleatorio(30);
-		 	}
-		 	var color = {r: aleatorio(250), g: aleatorio(250), b: aleatorio(250)};
-		 	dibujaCirculo(x,y,radio,color);
-		 }*/
 	};
 })();
 
-// Llamadas de ejemplo
 EJERCICIO([100, 20, 5], [30, 50], [80, 120]);
 EJERCICIO([150, 80], [60, 150, 30]);
